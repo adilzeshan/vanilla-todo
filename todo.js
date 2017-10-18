a197b10d75a8ce690cb92f768f68396eed7629b5
Adil Zeshan <adil@zeshan.org>
2017-10-18 23:52:03 +0100

    Add a method for deleting a todo

--- todo.js
+++ todo.js
@@ -10,5 +10,9 @@ const todoList = {
   changeTodo: function (position, newValue) {
     this.todos[position] = newValue;
     this.displayTodos();
+  },
+  deleteTodo: function (position) {
+    this.todos.splice(position, 1);
+    this.displayTodos();
   }
 };
