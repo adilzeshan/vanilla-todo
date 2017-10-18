1cba7223b909457fbe71ff3f23d29a6605c3fd7f
Adil Zeshan <adil@zeshan.org>
2017-10-18 23:32:55 +0100

    Add a method for adding a todo

--- todo.js
+++ todo.js
@@ -2,5 +2,9 @@ const todoList = {
   todos: [],
   displayTodos: function () {
     console.log('My Todos:', this.todos);
+  },
+  addTodo: function (todo) {
+    this.todos.push(todo);
+    this.displayTodos();
   }
 };
