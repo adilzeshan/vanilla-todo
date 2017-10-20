f128af51412227e61042e279a9c9005cfd6c5a47
Adil Zeshan <adil@zeshan.org>
2017-10-20 02:56:47 +0100

    Display message when todo list is empty

--- todo.js
+++ todo.js
@@ -1,27 +1,31 @@
 const todoList = {
   todos: [],
-  displayTodos: function() {
-    console.log('My Todos:');
-    for (var i = 0; i < this.todos.length; i++) {
-      console.log(this.todos[i].todoText);
+  displayTodos: function () {
+    if (this.todos.length === 0) {
+      console.log('Your todo list is empty.');
+    } else {
+      console.log('My Todos:');
+      for (var i = 0; i < this.todos.length; i++) {
+        console.log(this.todos[i].todoText);
+      }
     }
   },
-  addTodo: function(todoText) {
+  addTodo: function (todoText) {
     this.todos.push({
       todoText: todoText,
       completed: false
     });
     this.displayTodos();
   },
-  changeTodo: function(position, todoText) {
+  changeTodo: function (position, todoText) {
     this.todos[position].todoText = todoText;
     this.displayTodos();
   },
-  deleteTodo: function(position) {
+  deleteTodo: function (position) {
     this.todos.splice(position, 1);
     this.displayTodos();
   },
-  toggleCompleted: function(position) {
+  toggleCompleted: function (position) {
     const todo = this.todos[position];
     todo.completed = !todo.completed;
     this.displayTodos();
